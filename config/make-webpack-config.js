7d627ccb17f43cf438cb8b3b4e6741cf552e831e
Ernie Casilla <ecasilla@icloud.com>
2015-05-19 23:09:19 -0400

    creating a resuseable webpack conf

---
+++ config/make-webpack-config.js
@@ -0,0 +1,100 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = function(options) {
+  var cssLoaders = 'style-loader!css-loader!autoprefixer-loader?browsers=last 2 versions';
+  var sassLoaders = cssLoaders + '!sass-loader?indentedSyntax=sass';
+
+  if (options.production) {
+    cssLoaders = ExtractTextPlugin.extract('style-loader', cssLoaders.substr(cssLoaders.indexOf('!')));
+    sassLoaders = ExtractTextPlugin.extract('style-loader', sassLoaders.substr(sassLoaders.indexOf('!')));
+  }
+
+  var jsLoaders = ['babel-loader'];
+
+  return {
+    entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './src/index'
+    ],
+    debug: !options.production,
+    devtool: options.devtool,
+    output: {
+      path: options.production ? './dist' : './build',
+      publicPath: '',
+      filename: options.production ? 'app.[hash].js' : 'app.js',
+    },
+    module: {
+      preLoaders: options.lint ? [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader',
+        }
+      ] : [],
+      loaders: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loaders: jsLoaders,
+        },
+        {
+          test: /\.jsx$/,
+          exclude: /node_modules/,
+          loaders: options.production ? jsLoaders : ['react-hot-loader'].concat(jsLoaders),
+        },
+        {
+          test: /\.css$/,
+          loader: cssLoaders,
+        },
+        {
+          test: /\.sass$/,
+          loader: sassLoaders,
+        },
+        {
+          test: /\.png$/,
+          loader: "url-loader?limit=100000&mimetype=image/png",
+        },
+        {
+          test: /\.svg$/,
+          loader: "url-loader?limit=100000&mimetype=image/svg+xml",
+        },
+        {
+          test: /\.gif$/,
+          loader: "url-loader?limit=100000&mimetype=image/gif",
+        },
+        {
+          test: /\.jpg$/,
+          loader: "file-loader",
+        },
+      ]
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx'],
+    },
+    plugins: options.production ? [
+      // Important to keep React file size down
+      new webpack.DefinePlugin({
+        "process.env": {
+          "NODE_ENV": JSON.stringify("production")
+        }
+      }),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin(),
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+        }
+      }),
+      new ExtractTextPlugin("app.[hash].css"),
+      new HtmlWebpackPlugin({
+        template: './conf/tmpl.html'
+      }),
+    ] : []
+  };
+};
+
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var options = {devtool: 'eval',lint: true}
+var config = require('../config/webpack.config')(options);
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
